5750b3ff8e9318f02f28f8ee46fc475d8e16b03c
Zachary King <kingzach77@gmail.com>
2018-09-27 21:24:28 -0500

    cleaner, using goroutines for capture and streaming

--- main.go
+++ main.go
@@ -2,62 +2,68 @@ package main
 
 import (
 	"fmt"
+	"github.com/hybridgroup/mjpeg"
+	"gocv.io/x/gocv"
 	"image"
 	"image/color"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"sync"
+	"syscall"
+)
 
-	"gocv.io/x/gocv"
+var (
+	deviceID int
+	err      error
+	webcam   *gocv.VideoCapture
+	stream   *mjpeg.Stream
+	saveFile string
+	xmlFile  string
+	classifier gocv.CascadeClassifier
+	blue 	 color.RGBA
+)
+
+var (
+	img 	gocv.Mat
+	mut 	sync.Mutex
 )
 
 func main() {
 	defer func() { fmt.Println("shutting down...") }()
 	if len(os.Args) < 4 {
-		fmt.Println("How to run:\n\tgocam [camera ID] [classifier XML file] [output file]")
+		fmt.Println("How to run:\n\tgocam [camera ID] [classifier XML file] [host] [output file]")
 		return
 	}
 
 	// Parse arguments
-	deviceID, _ := strconv.Atoi(os.Args[1])
-	xmlFile := os.Args[2]
-	saveFile := os.Args[3]
+	deviceID, err = strconv.Atoi(os.Args[1])
+	xmlFile = os.Args[2]
+	host := os.Args[3]
+	saveFile = os.Args[4]
+
+	// Color for the rect when faces detected
+	blue = color.RGBA{0, 0, 255, 0}
 
 	// Open webcam
-	webcam, err := gocv.VideoCaptureDevice(int(deviceID))
+	webcam, err = gocv.VideoCaptureDevice(int(deviceID))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer webcam.Close()
 
-	// Open display window
-	window := gocv.NewWindow("GoCam")
-	defer window.Close()
-
 	// Prepare image matrix
-	img := gocv.NewMat()
+	img = gocv.NewMat()
 	defer img.Close()
 
-	// Color for the rect when faces detected
-	blue := color.RGBA{0, 0, 255, 0}
+	// Create the mjpeg stream
+	stream = mjpeg.NewStream()
 
-	// Initializes the image
-	if ok := webcam.Read(&img); !ok {
-		fmt.Printf("Cannot read device %v\n", deviceID)
-		return
-	}
-
-	// Open stream to write a file to
-	writer, err := gocv.VideoWriterFile(saveFile, "MJPG", 10, img.Cols(), img.Rows(), true)
-	if err != nil {
-		fmt.Printf("error opening video writer device: %v\n", saveFile)
-		fmt.Println(err)
-		return
-	}
-	defer writer.Close()
-
+	// Enable face detection
 	// Load classifier to recognize faces
-	classifier := gocv.NewCascadeClassifier()
+	classifier = gocv.NewCascadeClassifier()
 	defer classifier.Close()
 
 	if !classifier.Load(xmlFile) {
@@ -65,36 +71,62 @@ func main() {
 		return
 	}
 
-	fmt.Printf("start reading camera device: %v\n", deviceID)
+	// Capture a single image just to initialize the image variable
+	captureImage()
+
+	// Capture images from the camera in parallel
+	go func() {
+		for {
+			captureImage()
+			detectFaces()
+		}
+	}()
+
+	// Start capturing for mjpeg stream
+	go mjpegCapture()
+	fmt.Println("Streaming to " + host)
+
+	// Start HTTP Server
+	http.Handle("/", stream)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
+
+func detectFaces() {
+	mut.Lock()
+	defer mut.Unlock()
+
+	// Detect faces
+	rects := classifier.DetectMultiScale(img)
+	//fmt.Printf("found %d faces\n", len(rects))
+
+	// Draw a rectangle around each face on the original image,
+	// along with text identifying as "Human"
+	for _, r := range rects {
+		gocv.Rectangle(&img, r, blue, 3)
+		size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+		pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+		gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
+	}
+}
+
+func captureImage() {
+	mut.Lock()
+	defer mut.Unlock()
+
+	if ok := webcam.Read(&img); !ok {
+		fmt.Printf("Device closed: %v\n", deviceID)
+		syscall.Exit(-1)
+	}
+	if img.Empty() {
+		syscall.Exit(-1)
+	}
+}
+
+func mjpegCapture() {
 	for {
-		if ok := webcam.Read(&img); !ok {
-			fmt.Printf("cannot read device %d\n", deviceID)
-			return
-		}
-		if img.Empty() {
-			continue
-		}
-
-		// Detect faces
-		rects := classifier.DetectMultiScale(img)
-		//fmt.Printf("found %d faces\n", len(rects))
-
-		// Draw a rectangle around each face on the original image,
-		// along with text identifying as "Human"
-		for _, r := range rects {
-			gocv.Rectangle(&img, r, blue, 3)
-			size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
-			pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
-			gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
-		}
-
-		// Write the data to file
-		writer.Write(img)
-
-		// Show the image in the window and wait 1 millisecond
-		window.IMShow(img)
-		if window.WaitKey(1) >= 0 {
-			break
-		}
+		mut.Lock()
+		buf, _ := gocv.IMEncode(".jpg", img)
+		stream.UpdateJPEG(buf)
+		mut.Unlock()
 	}
 }
